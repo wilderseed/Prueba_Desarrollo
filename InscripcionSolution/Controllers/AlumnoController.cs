deffbd32e9d5aa0b0af02117dc869b285b7e4197
wilderSed <wilderSed@github.com>
2017-10-04 14:51:58 -0400

    Agregar archivos de proyecto.

---
+++ InscripcionSolution/Controllers/AlumnoController.cs
@@ -0,0 +1,21 @@
+﻿using InscripcionSolution.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InscripcionSolution.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            InscripcionContext DB = new InscripcionContext();
+            //List<Estudiante> listEstudiante = DB.Estudiante.Where(a => a.edad > 18).ToList();
+            List<Estudiante> listEstudiante = DB.Estudiante.ToList();
+            return View(listEstudiante);
+        }
+    }
+}
